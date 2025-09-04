c3622e317d79fb45fe18e7640689d7edc48f188d
arteksena <arteksena@gmail.com>
2025-09-04 14:51:14 -0500

    Delete cmdd.cmd

--- cmdd.cmd
+++
@@ -1,9 +0,0 @@
-git init
-git add .
-git commit -m "Mi primera versión de la página"
-git branch -M main
-git remote add origin https://github.com/arteksena/artek.github.io
-git push -u origin main
- git config --global user.email "you@example.com
-
-pause
